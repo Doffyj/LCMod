b55aebbc3d7d76c88fc55524d78f447d3d1db653
Wciv <sanchezeguiajon@gmail.com>
2023-12-13 07:56:38 +0100

    WIP: TBD

---
+++ Croissant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.Networking;
+using Croissant.Patches;
+
+
+namespace CroissantHoarderSFX
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class CroissantHoarder : BaseUnityPlugin
+    {
+        //from the video
+        private const string modGUID = "Doffyj.CroissantHoarderSFX";
+        private const string modName = "Croissant Hoarder SFX";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static CroissantHoarder Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null) 
+            {
+                Instance = this;
+            }
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo((object)"CroissantHoarderSFX: Checking the bakery....");
+            harmony.PatchAll();
+            mls.LogInfo((object)"CroissantHoarderSFX: Croissant Hoarders loaded!");
+        }
+    }
+}
---
+++ Patches/Patches.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Croissant.Patches
+{
+    [HarmonyPatch(typeof(HoarderBugAI))]
+    internal class Patches
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        public static void hoarderSFXpatch(ref AudioClip ___chitterSFX)
+        {
+            string filepath = "file://" + Paths.PluginPath + "\\CroissantHoarderSFX\\croisant.mp3";
+            UnityWebRequest loadSFX = UnityWebRequestMultimedia.GetAudioClip(filepath, (AudioType)13);
+            loadSFX.SendWebRequest();
+            while(!loadSFX.isDone) 
+            {
+            }
+            if ((int)loadSFX.result == 1)
+            {
+                ___chitterSFX = DownloadHandlerAudioClip.getContent(audioClip);
+            }
+        }
+    }
+}
